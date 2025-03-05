3cdd05d73baa7982a949179b0f16b75ca4011ec5
chencunge <chencunge@bytedance.com>
2025-03-05 12:04:07 +0800

    add lippiw

--- cgo.go
+++ cgo.go
@@ -7,7 +7,7 @@ package lilliput
 #cgo darwin CXXFLAGS: -I${SRCDIR}/deps/osx/include
 #cgo linux CXXFLAGS: -I${SRCDIR}/deps/linux/include
 #cgo darwin LDFLAGS: -L${SRCDIR}/deps/osx/lib -lavif -lyuv -laom -lavcodec -lavfilter -lavformat -lavutil -lbz2 -lgif -ljpeg -lopencv_core -lopencv_imgcodecs -lopencv_imgproc -lpng -lsharpyuv -lswscale -lwebp -lwebpmux -lz -framework CoreFoundation -framework CoreMedia -framework CoreVideo -framework VideoToolbox
-#cgo linux LDFLAGS: -L${SRCDIR}/deps/linux/lib -L${SRCDIR}/deps/linux/share/OpenCV/3rdparty/lib -lippicv -lswscale -lbz2 -lz -lopencv_core -lopencv_imgcodecs -lopencv_imgproc -ljpeg -lpng -lz -lgif -lopencv_core
+#cgo linux LDFLAGS: -L${SRCDIR}/deps/linux/lib -L${SRCDIR}/deps/linux/share/OpenCV/3rdparty/lib -lippicv -lippiw -liomp5 -lswscale -lbz2 -lz -lopencv_core -lopencv_imgcodecs -lopencv_imgproc -ljpeg -lpng -lz -lgif -lopencv_core
 void dummy() {}
 */
 import "C"
